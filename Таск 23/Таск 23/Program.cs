90d7c15e069ff6a1e3e6f40048977abff92e28ee
Kadyr505 <66276393+Kadyr505@users.noreply.github.com>
2021-10-05 00:53:44 +0300

    Add files via upload

---
+++ Таск 23/Таск 23/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Rextester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер матрицы: ");
+            int size = Int32.Parse(Console.ReadLine());
+
+            int[,] arr = new int[size, size];
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++)
+                    arr[i, j] = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < size; i++)
+            {
+                int sum = 0;
+                if (arr[i, i] % 2 != 0)
+                    continue;
+
+                for (int j = 0; j < size; j++)
+                    sum += arr[i, j];
+
+                Console.WriteLine("Сумма по строке [" + (i + 1) + "] = " + sum);
+            }
+        }
+    }
+}
